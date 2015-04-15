951769363414d2698a6990e40a70e5c422bf23fb
ttriggs <tyler.triggs@gmail.com>
2015-04-15 16:50:37 -0400

    add playlist show

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -6,18 +6,19 @@ class PlaylistsController < ApplicationController
   end
 
   def create
-    playlist = Playlist.find_or_initialize_by(user: current_user,
-                                              seed_artist: params[:playlist])
-    playlist.create_token = session[:token]["number"]
-    response = playlist.build_spotify_playlist
+    @playlist = Playlist.find_or_initialize_by(user: current_user,
+                                               seed_artist: params[:playlist])
+    @playlist.create_token = session[:token]["number"]
+    response = @playlist.build_spotify_playlist
 
     if response["snapshot_id"]
-      playlist.snapshot_id = response["snapshot_id"]
-      playlist.save!
+      @playlist.snapshot_id = response["snapshot_id"]
+      @playlist.save!
       flash[:notice] = "Playlist opened"
     else
       flash[:error] = "Failed to create playlist"
     end
+    redirect_to @playlist
   end
 
   private
