b4b35cef3697bfe63df746b4e57869ec29c3b83b
ttriggs <tyler.triggs@gmail.com>
2015-04-19 21:55:25 -0400

    correct seeder file for groups table

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ ifile = './generes_by_popularity.csv'
 File.open(ifile).readlines.each do |line|
   name, popularity, g1, g2, g3 = line.chomp.split(",")
   group_name = "#{g1}" + "#{g2}" + "#{g3}"
-  group = Group.find_or_create_by(name: group_name)
+  group = Group.find_or_create_by(rgb: group_name)
 
   Genre.find_or_create_by(name: name,
                           popularity: popularity,
