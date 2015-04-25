c7eeb42aed6ca499764993fefe44e8dcbab3e1ee
ttriggs <tyler.triggs@gmail.com>
2015-04-25 18:06:00 -0400

    move typeahead to own js folder/file

---
+++ app/assets/javascripts/typeahead/typeahead.js
@@ -0,0 +1,24 @@
+// Instantiate the Bloodhound suggestion engine
+var artists = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+        url: 'assets/js/artists.json',
+        filter: function (artists) {
+            return $.map(artists, function (artist) {
+                return {
+                    name: artist
+                };
+            });
+        }
+    }
+});
+
+// Initialize the Bloodhound suggestion engine
+artists.initialize();
+
+// Instantiate the Typeahead UI
+$('.typeahead').typeahead(null, {
+    displayKey: 'name',
+    source: artists.ttAdapter()
+});
