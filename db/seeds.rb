f6f45d38ded5c7400dbcdbfff3c1dde1f2f62a40
ttriggs <tyler.triggs@gmail.com>
2015-04-28 12:30:12 -0400

    refactor seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 
+# if old tables are missing datapoints or tacks are not linked to playlists
+# these are to update older versions of playlistor
+# to save track data for use in charts
 require_relative 'seeds_helper.rb'
 require_relative 'update_tables.rb'
+
 
 puts "ADDING GENRES TO DB:"
 ifile = './db/genres_by_popularity.csv'
@@ -14,36 +18,40 @@ File.open(ifile).readlines.each do |line|
                           group: group )
   print "+"
 end
+
 # initialize catch-all group for genres not in my seed file:
 Group.find_or_create_by(rgb: "(100, 100, 100)")
 Genre.find_or_create_by(name: "melancholia")
 
-#add follows to playlists created before follows_cache migration:
-Playlist.all.each do |playlist|
-  if playlist.follows_cache == 0
-    playlist.follows_cache = Follow.where(playlist_id: playlist.id).count
+
+all_playlists = Playlist.all
+if all_playlists.any?
+  #add follows to playlists created before follows_cache migration:
+  all_playlists.each do |playlist|
+    if playlist.follows_cache == 0
+      playlist.follows_cache = Follow.where(playlist_id: playlist.id).count
+      playlist.save!
+      print "pf+"
+    end
+  end
+
+  puts ">>>  record tracks for old playlists"
+  all_playlists.each do |playlist|
+    if playlist.has_no_tracks?
+      playlist.setup_new_tracklist
+      Hlpr.do_sleep(40)
+    else
+      puts ">>>  no need to update tracks"
+    end
+    puts "pt+"
+  end
+
+  # record tracks for old playlists
+  all_playlists.each do |playlist|
+    playlist.seed_artist = playlist.seed_artist.titleize
     playlist.save!
-    print "pf+"
+    puts "pSA+ "
   end
 end
-
-puts ">>>  record tracks for old playlists"
-Playlist.all.each do |playlist|
-  if playlist.has_no_tracks?
-    playlist.setup_new_tracklist
-    Hlpr.do_sleep(40)
-  else
-    puts ">>>  no need to update tracks"
-  end
-  puts "pt+"
-end
-
-# record tracks for old playlists
-Playlist.all.each do |playlist|
-  playlist.seed_artist = playlist.seed_artist.titleize
-  playlist.save!
-  puts "pSA+ "
-end
-
-puts ""
+puts "DONE"
 
