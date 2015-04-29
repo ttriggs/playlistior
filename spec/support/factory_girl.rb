7608bc74e895ea3069233a78d7da9ca2fdf1786a
ttriggs <tyler.triggs@gmail.com>
2015-04-28 22:35:25 -0400

    add factories for track, playlist

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,9 +2,20 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n| "user#{n}@example.com" }
-    password 'password'
-    password_confirmation 'password'
+    name SecureRandom.hex(10)
+  end
+
+  factory :track do
+    artist_name "bobs neato band"
+    spotify_id "29831hionfose"
+    title "my super neato song"
+    key 1
+    mode 0
+    tempo 120
+    danceability 0.344
+    energy 0.442
+    liveness 0.333
+    time_signature 4
   end
 
 end
