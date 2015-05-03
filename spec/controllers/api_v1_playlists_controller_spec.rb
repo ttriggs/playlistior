3ae300e3ed9bf7da3dd01875d3cc9c1348c69bae
ttriggs <tyler.triggs@gmail.com>
2015-05-03 17:50:56 -0500

    add cached chart test

--- spec/controllers/api_v1_playlists_controller_spec.rb
+++ spec/controllers/api_v1_playlists_controller_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 describe Api::V1::PlaylistsController do
   describe 'get #show' do
-    it 'responds with json for highcharts visualization' do
-      total_songs = 30
-      playlist = MockPlaylist.create(total_songs)
+    let(:total_songs) { 30 }
+    let(:playlist) { MockPlaylist.create(total_songs) }
+
+    it "responds with new json for highcharts if none cached" do
+      # total_songs = 30
+      # playlist = MockPlaylist.create(total_songs)
       session[:user_id] = playlist.user_id
       session[:token] = TokenFaker.get_fake_token
+      playlist.energy_json_cache = nil
 
       get :show, id: playlist.id
 
@@ -21,5 +25,17 @@ describe Api::V1::PlaylistsController do
       expect(chart_data).to have_content("Minor Songs")
       expect(chart_songs).to be(total_songs)
     end
+
+    it "responds with cached json if exists" do
+      playlist.energy_json_cache = MockChart.get_mock_chart
+      session[:user_id] = playlist.user_id
+      session[:token] = TokenFaker.get_fake_token
+
+      get :show, id: playlist.id
+
+      chart_data = response.stream.instance_values["buf"].first
+      expect(JSON.parse(chart_data)).to eq(JSON.parse(playlist.energy_json_cache))
+    end
+
   end
 end
