98b7bde2f0edac384f08df6dfe71deeeb1c7eb1d
ttriggs <tyler.triggs@gmail.com>
2015-05-02 21:30:18 -0500

    add playlists tests

--- spec/features/playlists_spec.rb
+++ spec/features/playlists_spec.rb
@@ -1,73 +1,44 @@
 require 'rails_helper'
 
-feature 'can view all created playlists' do
-  context 'get #index page' do
-    scenario 'see #index of playlists' do
-      playlist = MockPlaylist.create(30)
-      page.set_rack_session(user_id: playlist.user_id)
-      page.set_rack_session(token: TokenFaker.get_fake_token)
-
-      visit playlists_path
-      expect(page).to have_link(playlist.seed_artist.titleize)
-    end
+feature 'view/delete playlists' do
+  let(:playlist) { MockPlaylist.create(30) }
+  before(:each) do
+    page.set_rack_session(user_id: playlist.user_id)
+    page.set_rack_session(token: TokenFaker.get_fake_token)
   end
-end
 
-feature 'can view a specific playlist' do
   context 'get #show page' do
-    scenario 'see #show for playlist' do
-      playlist = MockPlaylist.create(30)
-      page.set_rack_session(user_id: playlist.user_id)
-      page.set_rack_session(token: TokenFaker.get_fake_token)
-
+    scenario 'view content playlist' do
       visit playlist_path(playlist)
       expect(page).to have_content("Inspired by #{playlist.seed_artist.titleize}")
       expect(page).to have_content("This harmonic playlist visualized")
       expect(page).to have_button("Extend")
       expect(page).to have_link("Delete")
     end
-  end
-end
 
-feature 'I can delete my playlist' do
-  context 'on #show page' do
-    scenario 'click delete button to remove playlist' do
-      playlist = MockPlaylist.create(30)
-      page.set_rack_session(user_id: playlist.user_id)
-      page.set_rack_session(token: TokenFaker.get_fake_token)
-
+    scenario 'I can delete my playlist' do
       visit playlist_path(playlist)
       click_on("Delete")
       expect(page).to have_content("Playlist Deleted")
     end
-  end
-end
 
-feature 'I cannot modify other users playlist' do
-  context 'on #show page' do
-    scenario 'click delete button to remove playlist' do
-      playlist = MockPlaylist.create(30)
+    scenario 'I cannot modify other users playlist' do
       user = FactoryGirl.create(:user)
       page.set_rack_session(user_id: user.id)
-      page.set_rack_session(token: TokenFaker.get_fake_token)
-
       visit playlist_path(playlist)
       expect(page).to have_button("Follow")
       expect(page).to_not have_button("Extend")
       expect(page).to_not have_link("Delete")
     end
   end
-end
 
+  context 'get #index page' do
+    scenario 'see #index of playlists' do
+      visit playlists_path
+      expect(page).to have_link(playlist.seed_artist.titleize)
+    end
 
-feature 'I can search playlists by genre' do
-  context 'on #index page' do
-    scenario 'click search by genre' do
-      playlist = MockPlaylist.create(30)
-      user = FactoryGirl.create(:user)
-      page.set_rack_session(user_id: user.id)
-      page.set_rack_session(token: TokenFaker.get_fake_token)
-
+    scenario 'see search by genre button' do
       visit playlists_path
       expect(page).to have_link("Search by Genre »")
       click_on("Search by Genre »", match: :first)
