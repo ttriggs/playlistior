678203f7e202d17ee9f9082abc2b434a0d4a3c6d
ttriggs <tyler.triggs@gmail.com>
2015-05-03 18:02:14 -0500

    minor cleanup

--- spec/support/chart_faker.rb
+++ spec/support/chart_faker.rb
@@ -1,7 +1,6 @@
 class MockChart
   CHARTS_FILE = File.expand_path("../example_chart.json", __FILE__)
   CHARTS_JSON = File.read(CHARTS_FILE)
-  # CHARTS_HASH = JSON.parse(CHARTS_JSON)
   def self.get_mock_chart
     CHARTS_JSON
   end
