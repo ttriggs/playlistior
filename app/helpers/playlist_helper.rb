4ed003a173a2452923dfee14e9f9733aa41588d6
ttriggs <tyler.triggs@gmail.com>
2015-05-03 18:03:10 -0500

    remove unnecessary helper methods

--- app/helpers/playlist_helper.rb
+++ app/helpers/playlist_helper.rb
@@ -3,10 +3,6 @@ module PlaylistHelper
     Genre.joins(:playlists).order('group_id asc').uniq
   end
 
-  def playlists_by_genre(genre)
-    Genre.find_by(name: genre).playlists
-  end
-
   def get_their_playlists
     Playlist.where.not(user_id: current_user.id).
                    order(follows_cache: :desc).
