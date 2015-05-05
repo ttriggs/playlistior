f531dd63a96fc2837647cac2cfc85cbc19c8ccb1
ttriggs <tyler.triggs@gmail.com>
2015-05-05 16:34:20 -0500

    remove country from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504221136) do
+ActiveRecord::Schema.define(version: 20150505204813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20150504221136) do
     t.string   "spotify_id",                     null: false
     t.string   "spotify_link",  default: ""
     t.string   "image"
-    t.string   "country",       default: ""
     t.string   "role",          default: "user"
     t.datetime "created_at"
     t.datetime "updated_at"
