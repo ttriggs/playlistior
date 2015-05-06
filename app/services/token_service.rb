ed53fbfc8f57dc3a0d35435a87141da367980294
ttriggs <tyler.triggs@gmail.com>
2015-05-06 00:05:04 -0500

    comment out unnecessary code

--- app/services/token_service.rb
+++ app/services/token_service.rb
@@ -1,15 +1,5 @@
 class TokenService
 
-  # def self.new_tokens(code)
-  #   response = connection.post "/api/token" do |request|
-  #     request.headers = auth_header
-  #     request.body = { grant_type: "authorization_code",
-  #                      code: code,
-  #                      redirect_uri: redirect_uri }
-  #   end
-  #   parse(response)
-  # end
-
   def self.refresh_token
     response = connection.post "/api/token" do |request|
       request.headers = { "Authorization" => "Basic #{encoded_id_and_secret}" }
@@ -40,12 +30,4 @@ class TokenService
   def self.client_secret
     ENV["SPOTIFY_SECRET"]
   end
-
-  # def self.scope
-  #   CGI.escape("user-read-email user-read-private playlist-modify-private playlist-read-private")
-  # end
-
-  # def self.redirect_uri
-  #   ENV['SPOTIFY_CALLBACK_URI']
-  # end
 end
