6f9153b6392371952e35b2d1cd86de613a476199
ttriggs <tyler.triggs@gmail.com>
2015-05-05 20:36:01 -0500

    remove unnecessary code

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,21 +19,6 @@ class User < ActiveRecord::Base
     user
   end
 
-  # def self.fetch_or_build_user(session)
-  #   user_data = get_user_data(session)
-  #   user = User.find_or_initialize_by(spotify_id: user_data["id"] )
-  #   unless user.persisted?
-  #     user.email = user_data["email"]
-  #     user.name  = user_data["display_name"] || user.email
-  #     user.image = user.get_image(user_data["images"])
-  #     user.country = user_data["country"]
-  #     user.spotify_link = user_data["href"]
-  #   end
-  #   user.session_token = session[:token][:number]
-  #   user.refresh_token = session[:token][:refresh]
-  #   user
-  # end
-
   def self.fetch_or_build_user_for_view(session)
     if session[:token] && User.exists?(session[:user_id])
       User.find(session[:user_id])
@@ -64,12 +49,4 @@ class User < ActiveRecord::Base
   def guest?
     false
   end
-
-  private
-
-  # def self.get_user_data(session)
-  #   access_token = session[:token][:number]
-  #   params = { headers: { "Authorization" => "Bearer #{access_token}"} }
-  #   HTTParty.get("https://api.spotify.com/v1/me", params)
-  # end
 end
