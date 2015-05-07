f1d5ef2bd7829f186c6a23a9977acb459ae21d30
ttriggs <tyler.triggs@gmail.com>
2015-05-07 12:27:18 -0500

    mock auth helper

--- spec/support/omniauth_mock.rb
+++ spec/support/omniauth_mock.rb
@@ -3,17 +3,27 @@ class OmniauthMock
 
   def self.setup_login
     prepare_test
-    OmniauthMock.valid_credentials
+    self.valid_credentials
   end
 
   def self.setup_invalid_login
     prepare_test
-    OmniauthMock.invalid_credentials
+    self.invalid_credentials
   end
+
+  def self.setup_no_uid_invalid_login
+    prepare_test
+    self.no_uid_invalid_credentials
+  end
+
 
   def self.prepare_test
     OmniAuth.config.test_mode = true
-    OmniauthMock.clean!
+    OmniAuth.config.mock_auth[:spotify] = nil
+  end
+
+  def self.invalid_credentials
+    OmniAuth.config.mock_auth[:spotify] = :invalid_credentials
   end
 
   def self.valid_credentials
@@ -37,11 +47,25 @@ class OmniauthMock
             })
   end
 
-  def self.invalid_credentials
-    OmniAuth.config.mock_auth[:spotify] = :invalid_credentials
+  def self.no_uid_invalid_credentials
+    OmniAuth.config.mock_auth[:spotify] = OmniAuth::AuthHash.new({
+              provider: 'spotify',
+              info: {
+                name: "1337_haxor",
+                image: "www.myface.com/123.png",
+                email: "myface@hullo.com"
+              },
+              credentials: {
+                token: "token",
+                refresh_token: "refresh_token"
+              },
+              extra: {
+                raw_info: {
+                  href: "my_spotify_link.com"
+                }
+              }
+            })
   end
 
-  def self.clean!
-    OmniAuth.config.mock_auth[:spotify] = nil
-  end
+
 end
