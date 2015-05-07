cc86bac9f3e2406e9967c073836867fa18ce6bc4
ttriggs <tyler.triggs@gmail.com>
2015-05-07 12:19:15 -0500

    create test and path for failing spotify auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_APP_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-read-email user-read-private playlist-modify-private playlist-read-private'
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
