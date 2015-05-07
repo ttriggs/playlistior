e8aa0170d598a4ab9a3727f717c3e763e7afa30c
ttriggs <tyler.triggs@gmail.com>
2015-05-07 16:10:23 -0500

    refactor flash messages

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -9,14 +9,15 @@ class PlaylistsController < ApplicationController
   def create
     playlist_creator = PlaylistCreator.new(params, current_user)
     playlist_creator.create if playlist_creator.no_errors?
+    set_create_flash(playlist_creator.flash_errors)
     if playlist_creator.success?
-      flash[:success] = "Playlist generated (updates may appear first in Spotify app :)"
+      # flash[:success] = "Playlist generated (updates may appear first in Spotify app :)"
       redirect_to playlist_path(playlist_creator.playlist)
     elsif playlist_creator.notice?
-      flash[:notice] = playlist_creator.flash_errors[:notice]
+      # flash[:notice] = playlist_creator.flash_errors[:notice]
       redirect_to playlist_path(playlist_creator.playlist)
     elsif playlist_creator.exit_error?
-      flash[:errors] = playlist_creator.flash_errors[:errors]
+      # flash[:errors] = playlist_creator.flash_errors[:errors]
       playlist_creator.playlist.destroy if playlist_creator.playlist_invalid?
       redirect_to playlists_path
     end
@@ -38,5 +39,17 @@ class PlaylistsController < ApplicationController
 
   def show
     @playlist = Playlist.find(params[:id])
+  end
+
+  private
+
+  def set_flash(flash_errors)
+    if flash_errors[:errors]
+      flash[:errors] = flash_errors[:errors]
+    elsif flash_errors[:notice]
+      flash[:notice] = flash_errors[:notice]
+    else
+      flash[:success] = "Playlist generated (updates may appear first in Spotify app :)"
+    end
   end
 end
