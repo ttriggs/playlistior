f037f6dcde9d8926d353d29dd95f4b75cd0420a1
ttriggs <tyler.triggs@gmail.com>
2015-05-30 15:58:05 -0400

    refactor methods

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -3,13 +3,18 @@ class Api::V1::PlaylistsController < ApplicationController
   def show
     playlist = Playlist.find(params[:id])
     quality = get_quality_from_params(params[:quality])
-    render json: playlist.find_or_create_chart_data(quality),
+    chart_cache_symbol = cache_symbol_from(quality)
+    render json: playlist.find_or_create_chart_data(quality, chart_cache_symbol),
            callback: params[:callback]
   end
 
   private
 
-  def get_quality_from_params(quality)
-    quality.nil? ? :energy : quality.to_sym
-  end
+    def cache_symbol_from(quality)
+      (quality.to_s + "_json_cache").to_sym
+    end
+
+    def get_quality_from_params(quality)
+      quality.nil? ? :energy : quality.to_sym
+    end
 end
