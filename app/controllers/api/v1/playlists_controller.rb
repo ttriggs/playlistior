993964fd9c55425c98c1b0135d6af0f85be42fda
ttriggs <tyler.triggs@gmail.com>
2015-05-30 15:27:45 -0400

    move methods to playlist model

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,79 +2,14 @@ class Api::V1::PlaylistsController < ApplicationController
 
   def show
     playlist = Playlist.find(params[:id])
-    type = get_type_from_params(params[:type])
-    json_cache_symbol = type_to_json_cache_symbol(type)
-    if playlist[json_cache_symbol]
-      @playlist_data = playlist[json_cache_symbol]
-    else
-      @playlist_data = get_playlist_json(playlist, type)
-      playlist[json_cache_symbol] = JSON(@playlist_data)
-      playlist.save! # move this to model later
-    end
-    render json: @playlist_data, callback: params[:callback]
+    quality = get_quality_from_params(params[:quality])
+    render json: playlist.find_or_create_chart_data(quality),
+           callback: params[:callback]
   end
 
   private
 
-  def get_playlist_json(playlist, type=:energy)
-    active_tracks = playlist.active_tracks_in_order
-    @major_series = []
-    @minor_series = []
-    active_tracks.each do |track_hash|
-      track = track_hash[:track]
-      order = track_hash[:order]
-      circle_zone = Camelot.get_circle_zone(track)
-      energy = scaled_value(track[type])
-      if track.key == 1
-        @major_series << [order, circle_zone, energy]
-      else
-        @minor_series << [order, circle_zone, energy]
-      end
-    end
-    create_chart_json(playlist, type, @major_series, @minor_series)
-  end
-
-  def create_chart_json(playlist, type, major_series, minor_series)
-    {
-      chart: {
-             type: 'bubble',
-             zoomType: 'xy'
-             },
-      title: {
-        text: "#{playlist.seed_artist} playlist visualized"
-      },
-      subtitle: {
-        text: "bubble size: relative song #{type.to_s}"
-      },
-      xAxis: {
-        title: { text: 'Play Order' }
-      },
-      yAxis: {
-         allowDecimals: false,
-         title: { text: 'Camelot Zone' }
-      },
-      series: [
-        {
-          name: 'Major Songs',
-          data: major_series
-        },
-        {
-          name: 'Minor Songs',
-          data: minor_series
-        }
-      ]
-    }
-  end
-
-  def get_type_from_params(type)
-    type.nil? ? :energy : type.to_sym
-  end
-
-  def type_to_json_cache_symbol(type)
-    (type.to_s + "_json_cache").to_sym
-  end
-
-  def scaled_value(value)
-    value < 1 ? (value * 100_000).to_i : value
+  def get_quality_from_params(quality)
+    quality.nil? ? :energy : quality.to_sym
   end
 end
