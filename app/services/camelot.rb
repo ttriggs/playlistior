e28648746f129d7c876326d980e819ee376b05e8
ttriggs <tyler.triggs@gmail.com>
2015-06-17 15:25:59 -0500

    slight reformat, better method naming

--- app/services/camelot.rb
+++ app/services/camelot.rb
@@ -1,42 +1,46 @@
 class Camelot
     # echonest-specific key naming
-    EN_KEYS  = { 0 => "C",
-                  1 => "Db",
-                  2 => "D",
-                  3 => "Eb",
-                  4 => "E",
-                  5 => "F",
-                  6 => "F#",
-                  7 => "G",
-                  8 => "Ab",
-                  9 => "A",
-                  10 => "Bb",
-                  11 => "B" }
+    EN_KEYS  = {
+                 0 => "C",
+                 1 => "Db",
+                 2 => "D",
+                 3 => "Eb",
+                 4 => "E",
+                 5 => "F",
+                 6 => "F#",
+                 7 => "G",
+                 8 => "Ab",
+                 9 => "A",
+                 10 => "Bb",
+                 11 => "B"
+               }
     # coded circle of fifths
-    CIRCLE = { ["B", 1] => 1,
-                 ["Ab", 0] => 1,
-                 ["F#", 1] => 2,
-                 ["Eb", 0] => 2,
-                 ["Db", 1] => 3,
-                 ["Bb", 0] => 3,
-                 ["Ab", 1] => 4,
-                 ["F", 0] => 4,
-                 ["Eb", 1] => 5,
-                 ["C", 0] => 5,
-                 ["Bb", 1] => 6,
-                 ["G", 0] => 6,
-                 ["F", 1] => 7,
-                 ["D", 0] => 7,
-                 ["C", 1] => 8,
-                 ["A", 0] => 8,
-                 ["G", 1] => 9,
-                 ["E", 0] => 9,
-                 ["D", 1] => 10,
-                 ["B", 0] => 10,
-                 ["A", 1] => 11,
-                 ["F#", 0] => 11,
-                 ["E", 1] => 12,
-                 ["Db", 0] => 12 }
+    CIRCLE = {
+               ["B", 1] => 1,
+               ["Ab", 0] => 1,
+               ["F#", 1] => 2,
+               ["Eb", 0] => 2,
+               ["Db", 1] => 3,
+               ["Bb", 0] => 3,
+               ["Ab", 1] => 4,
+               ["F", 0] => 4,
+               ["Eb", 1] => 5,
+               ["C", 0] => 5,
+               ["Bb", 1] => 6,
+               ["G", 0] => 6,
+               ["F", 1] => 7,
+               ["D", 0] => 7,
+               ["C", 1] => 8,
+               ["A", 0] => 8,
+               ["G", 1] => 9,
+               ["E", 0] => 9,
+               ["D", 1] => 10,
+               ["B", 0] => 10,
+               ["A", 1] => 11,
+               ["F#", 0] => 11,
+               ["E", 1] => 12,
+               ["Db", 0] => 12
+             }
 
   def initialize(uris, tracks)
     @uris = uris
@@ -56,7 +60,7 @@ class Camelot
     tracklist = [take_random(@full_tracklist)]
     until hit_song_limit?(tracklist)
       last_song_params = get_key_and_mode(tracklist.last)
-      neighbors_params = get_neighbor_params(last_song_params)
+      neighbors_params = get_neighbor_params_array(last_song_params)
       tracklist << get_next_song(neighbors_params)
     end
     tracklist
@@ -67,10 +71,10 @@ class Camelot
       @full_tracklist.empty?
   end
 
-  def get_neighbor_params(params_array)
+  def get_neighbor_params_array(params_array)
     track_key, mode = params_array
-    key = EN_KEYS[track_key]
-    zone  = CIRCLE[[key, mode]]
+    key  = EN_KEYS[track_key]
+    zone = CIRCLE[[key, mode]]
     close_zones = CIRCLE.select {|_key, circle_zone| circle_zone == zone }
     near_zones = CIRCLE.select do |key, circle_zone|
       circle_zone.between?(zone - 1, zone +1) && key[1] == mode
