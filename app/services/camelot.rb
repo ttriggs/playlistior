389de76c464f29cd1e82b3d4dae071a753b4e92a
ttriggs <tyler.triggs@gmail.com>
2015-06-30 18:44:38 -0500

    add 'escalator' mode to continually increment camelot zone

--- app/services/camelot.rb
+++ app/services/camelot.rb
@@ -16,52 +16,46 @@ class Camelot
                }
     # coded circle of fifths
     CIRCLE = {
-               ["B", 1] => 1,
-               ["Ab", 0] => 1,
-               ["F#", 1] => 2,
-               ["Eb", 0] => 2,
-               ["Db", 1] => 3,
-               ["Bb", 0] => 3,
-               ["Ab", 1] => 4,
-               ["F", 0] => 4,
-               ["Eb", 1] => 5,
-               ["C", 0] => 5,
-               ["Bb", 1] => 6,
-               ["G", 0] => 6,
-               ["F", 1] => 7,
-               ["D", 0] => 7,
-               ["C", 1] => 8,
-               ["A", 0] => 8,
-               ["G", 1] => 9,
-               ["E", 0] => 9,
-               ["D", 1] => 10,
-               ["B", 0] => 10,
-               ["A", 1] => 11,
-               ["F#", 0] => 11,
-               ["E", 1] => 12,
-               ["Db", 0] => 12
+               ["B", 1] => 0,
+               ["Ab", 0] => 0,
+               ["F#", 1] => 1,
+               ["Eb", 0] => 1,
+               ["Db", 1] => 2,
+               ["Bb", 0] => 2,
+               ["Ab", 1] => 3,
+               ["F", 0] => 3,
+               ["Eb", 1] => 4,
+               ["C", 0] => 4,
+               ["Bb", 1] => 5,
+               ["G", 0] => 5,
+               ["F", 1] => 6,
+               ["D", 0] => 6,
+               ["C", 1] => 7,
+               ["A", 0] => 7,
+               ["G", 1] => 8,
+               ["E", 0] => 8,
+               ["D", 1] => 9,
+               ["B", 0] => 9,
+               ["A", 1] => 10,
+               ["F#", 0] => 10,
+               ["E", 1] => 11,
+               ["Db", 0] => 11
              }
 
-  def initialize(uris, tracks)
-    @uris = uris
-    @full_tracklist = get_array_of_new_tracks(tracks)
-  end
-
-  def get_array_of_new_tracks(tracks)
-    tracks.each_with_object(new_tracks = []) do |track|
-      unless @uris.include?(track.spotify_id.to_s)
-        new_tracks << track
-      end
-    end
+  def initialize(full_tracklist, selection_method = :wander)
+    @full_tracklist = full_tracklist
+    @selection_method = selection_method
   end
 
   def order_tracks
-    return @full_tracklist if @full_tracklist.empty?
-    tracklist = [take_random(@full_tracklist)]
+    return [] if @full_tracklist.empty?
+    tracklist = [@full_tracklist.take_random]
     until hit_song_limit?(tracklist)
-      last_song_params = get_key_and_mode(tracklist.last)
-      neighbors_params = get_neighbor_params_array(last_song_params)
-      tracklist << get_next_song(neighbors_params)
+      @last_song_params = get_key_and_mode(tracklist.last)
+      @last_song_zone   = Camelot.get_camelot_zone(tracklist.last)
+      @neighbors_params = get_neighbor_params_array
+      next_song = get_next_song
+      tracklist << next_song
     end
     tracklist
   end
@@ -71,24 +65,20 @@ class Camelot
       @full_tracklist.empty?
   end
 
-  def get_neighbor_params_array(params_array)
-    track_key, mode = params_array
+  def get_neighbor_params_array
+    track_key, mode = @last_song_params
     key  = EN_KEYS[track_key]
     zone = CIRCLE[[key, mode]]
     close_zones = CIRCLE.select {|_key, circle_zone| circle_zone == zone }
-    near_zones = CIRCLE.select do |key, circle_zone|
+    adjacent_zones = CIRCLE.select do |key, circle_zone|
       circle_zone.between?(zone - 1, zone +1) && key[1] == mode
     end
-    all_zones = close_zones.merge(near_zones).keys
+    all_zones = close_zones.merge(adjacent_zones).keys
     all_zones.map! {|key, mode| [EN_KEYS.key(key), mode] }
   end
 
   def get_key_and_mode(track)
     [track.key, track.mode]
-  end
-
-  def take_random(array)
-    array.delete_at(rand(array.length))
   end
 
   def next_song_by_params(song_params)
@@ -99,17 +89,39 @@ class Camelot
     end
   end
 
-  def get_next_song(neighbors_params)
-    index = -1
-    neighbors_params.shuffle.each do |song_params|
-      index = next_song_by_params(song_params) || -1
-      return take_song(index) if index > -1
+  def get_next_song
+    next_song = method(@selection_method).call
+    if next_song.class != Track
+      next_song = @full_tracklist.take_random
     end
-    take_song(index)
+    next_song
   end
 
-  def take_song(index)
-    @full_tracklist.delete_at(index)
+  def escalator
+    next_zone = (@last_song_zone + 1 ) % 12
+    next_mode = @last_song_params[1]
+    next_key  = escalate_key(next_zone, next_mode)
+    next_song_params = [next_key, next_mode]
+    if @full_tracklist.track_exists?(next_song_params)
+      @full_tracklist.take_track(next_song_params)
+    else
+      wander
+    end
+  end
+
+  def escalate_key(next_zone, next_mode)
+    zone = CIRCLE.select do |key, zone|
+      zone == next_zone && key[1] == next_mode
+    end
+    EN_KEYS.invert[zone.keys.first[0]]
+  end
+
+  def wander
+    @neighbors_params.shuffle.each do |song_params|
+      if @full_tracklist.track_exists?(song_params)
+        return @full_tracklist.take_track(song_params)
+      end
+    end
   end
 
   def self.get_camelot_zone(track)
